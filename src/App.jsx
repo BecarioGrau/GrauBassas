1c0e5539b51ce92b8c4d96269befa6d26b446e56
Juanda <becariograubassas@gmail.com>
2025-12-23 09:44:01 +0000

    implementation of reouter dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import Footer from './components/Footer'
 import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      
       <Footer />
     </>
   )
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,3 @@
+export default function Contact() {
+    return <div className="bg-primary text-white"><h1>Contact Page</h1></div>;
+}
