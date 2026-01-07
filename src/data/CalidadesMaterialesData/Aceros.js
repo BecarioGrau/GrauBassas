6bdf7d81d1696d57ec6d2a96d90db58853b86d82
Juanda <becariograubassas@gmail.com>
2026-01-07 10:15:41 +0000

    new data structure

---
+++ src/data/CalidadesMaterialesData/Aceros.js
@@ -0,0 +1,156 @@
+export const aceros = [
+  {
+    id: 1,
+    title: "F-1110",
+    description:
+      "Calidad de acero F-1110 pertenece a la categoría de aceros al carbono de construcción, específicamente de bajo contenido en carbono.Este acero se utiliza comúnmente para piezas que requieren buena tenacidad y facilidad de soldadura, como clavos, alambres, herrajes y piezas para embutición en frío o plegado",
+    tableConfig: {
+      columns: [
+        {
+          header: "Calidad",
+          key: "quality",
+          rowSpan: 2,
+        },
+        {
+          header: "Composición",
+          colSpan: 8,
+          subColumns: [
+            { header: "C", key: "c" },
+            { header: "Mn", key: "mn" },
+            { header: "Si", key: "si" },
+            { header: "Cr", key: "cr" },
+            { header: "Ni", key: "ni" },
+            { header: "Mo", key: "mo" },
+            { header: "V", key: "v" },
+            { header: "W.-Nr.", key: "wnr" },
+          ],
+        },
+        {
+          header: "Equivalencias",
+          colSpan: 3,
+          subColumns: [
+            { header: "DIN", key: "din" },
+            { header: "AISI", key: "aisi" },
+            { header: "SAE", key: "sae" },
+          ],
+        },
+      ],
+      rows: [
+        {
+          quality: "F-1110",
+          c: "0.10",
+          mn: "0.40",
+          si: "0.15",
+          cr: "—",
+          ni: "—",
+          mo: "—",
+          v: "—",
+          wnr: "1.1141",
+          din: "1.1141",
+          aisi: "CK-15",
+          sae: "1015",
+        },
+      ],
+    },
+    cortes: [
+      {
+        label: "Macizo",
+        href: "#",
+        image:
+          "https://lh3.googleusercontent.com/aida-public/AB6AXuCeRG5RbQWugsVNcludm6LB_R3egwLO3bsZujCXgFUk-zvBjVNgu7NUaNiftVSdXhR7re94h85qhW948UhhIS_pb3HSPiCoG40_PJv4l1tXsTf7LP22L0bPlI4_syHXR4G7Hnz5bRhwUW4ELEMzYXgOuG3f4gtLCFh6xMHWtvqaPKFyfs7VVaIV_4TOOn5NFnsyz0HW1ELupoBOvQEcD0eMeEhuVqizajPMA01h1z-2EvzOQREhsB-qnUmUC-KdfUTmQKMhDKzv9LKS",
+      },
+      {
+        label: "Chapa",
+        href: "#",
+        image:
+          "https://lh3.googleusercontent.com/aida-public/AB6AXuAmQLmBiWBCNfci0bwcxKGzYv1PWBH1iOyCuNZEsGjAiB9q6m1_yg7WHVTcfevQuewzd3l8AKP5EKZCa1Q90aHxgUiApYGZmoKGObrZlFgJdX_PrQB_a4Uqmz4-exeBMh0zF7EC80ZfMvTqDxfvtOaPFnOtMKvSU6wFmn9h3kjyOZMj7HlK6lBFB9c7AbpaFasuZoDOLixEK4q2XUFu5yRJD-BU-2KWLw426ugSuJP-xnAf6s7QTOo8zXwcRsxVeEWeHCrNWrV9Kc92",
+      },
+      {
+        label: "Ángulo",
+        href: "#",
+        image:
+          "https://lh3.googleusercontent.com/aida-public/AB6AXuCMJ5-HtUNFlHeNLzpWMEg61Y7rlk_zFSg_kE2UzBv7j0l-pC7rgQcIUNRc0wFc2prPU2tSD8Vbezbvq3ARk48JsXKBB2gWM9vbRvp16MQa6WqTfJZG2x1xn3BzXLDDuxhzrM03meBgvHYbvUhAfAgPNmaXMh1nwazSP1v6CKnc8KAGa6GAevs3QynGm9S1SdRYaSEBd60im_gE0KTpEsNFToPQrgg7bezYblZiqMskY-wgF6h8vTJ3iqRgWsXKPe9o2Q5oe0ovNcp4",
+      },
+      {
+        label: "Cuadrado",
+        href: "#",
+        image:
+          "https://lh3.googleusercontent.com/aida-public/AB6AXuA264JCnJCUrE4IzS4ctV-e-wxbCEFJFKrtcK3Y_CKXeqn6XoOevE41vReZVIujGEzCTfIJrWUyGZkG7ljUWAs8KFPInfBsU0KAi5__MDIl1rrUENDNdXrLcxXY25j_3ZUAkUv--FHLezgMI6doB9LQUn196PBIFIfbwjSFuLb4lfyjTc4McldMe5dxzkTZgKNos3lM8yaZZVoPtNt48yYaH1Zwv44yMYADzp9ashriZaYDN4U7eZ1xVEjYPKlVYRMKV3rb1MkNMbnl",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "F-1120",
+    description:
+      "Calidad de acero F-1120 ESTO ES UNA PRUEBA PARA VER SI FUNCIONA",
+    tableConfig: {
+      columns: [
+        {
+          header: "Calidad",
+          key: "quality",
+          rowSpan: 2,
+        },
+        {
+          header: "Composición",
+          colSpan: 8,
+          subColumns: [
+            { header: "C", key: "c" },
+            { header: "Mn", key: "mn" },
+            { header: "Si", key: "si" },
+            { header: "Cr", key: "cr" },
+            { header: "Ni", key: "ni" },
+            { header: "Mo", key: "mo" },
+            { header: "V", key: "v" },
+            { header: "W.-Nr.", key: "wnr" },
+          ],
+        },
+        {
+          header: "Equivalencias",
+          colSpan: 3,
+          subColumns: [
+            { header: "DIN", key: "din" },
+            { header: "AISI", key: "aisi" },
+            { header: "SAE", key: "sae" },
+          ],
+        },
+      ],
+      rows: [
+        {
+          quality: "F-1120",
+          c: "0.10",
+          mn: "0.40",
+          si: "0.15",
+          cr: "—",
+          ni: "—",
+          mo: "—",
+          v: "—",
+          wnr: "1.1141",
+          din: "1.1141",
+          aisi: "CK-15",
+          sae: "1015",
+        },
+      ],
+    },
+    cortes: [
+      {
+        label: "Macizo",
+        href: "#",
+        image:
+          "https://lh3.googleusercontent.com/aida-public/AB6AXuCeRG5RbQWugsVNcludm6LB_R3egwLO3bsZujCXgFUk-zvBjVNgu7NUaNiftVSdXhR7re94h85qhW948UhhIS_pb3HSPiCoG40_PJv4l1tXsTf7LP22L0bPlI4_syHXR4G7Hnz5bRhwUW4ELEMzYXgOuG3f4gtLCFh6xMHWtvqaPKFyfs7VVaIV_4TOOn5NFnsyz0HW1ELupoBOvQEcD0eMeEhuVqizajPMA01h1z-2EvzOQREhsB-qnUmUC-KdfUTmQKMhDKzv9LKS",
+      },
+      {
+        label: "Chapa",
+        href: "#",
+        image:
+          "https://lh3.googleusercontent.com/aida-public/AB6AXuAmQLmBiWBCNfci0bwcxKGzYv1PWBH1iOyCuNZEsGjAiB9q6m1_yg7WHVTcfevQuewzd3l8AKP5EKZCa1Q90aHxgUiApYGZmoKGObrZlFgJdX_PrQB_a4Uqmz4-exeBMh0zF7EC80ZfMvTqDxfvtOaPFnOtMKvSU6wFmn9h3kjyOZMj7HlK6lBFB9c7AbpaFasuZoDOLixEK4q2XUFu5yRJD-BU-2KWLw426ugSuJP-xnAf6s7QTOo8zXwcRsxVeEWeHCrNWrV9Kc92",
+      },
+      {
+        label: "Ángulo",
+        href: "#",
+        image:
+          "https://lh3.googleusercontent.com/aida-public/AB6AXuCMJ5-HtUNFlHeNLzpWMEg61Y7rlk_zFSg_kE2UzBv7j0l-pC7rgQcIUNRc0wFc2prPU2tSD8Vbezbvq3ARk48JsXKBB2gWM9vbRvp16MQa6WqTfJZG2x1xn3BzXLDDuxhzrM03meBgvHYbvUhAfAgPNmaXMh1nwazSP1v6CKnc8KAGa6GAevs3QynGm9S1SdRYaSEBd60im_gE0KTpEsNFToPQrgg7bezYblZiqMskY-wgF6h8vTJ3iqRgWsXKPe9o2Q5oe0ovNcp4",
+      },
+    ],
+  },
+];
