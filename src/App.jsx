63003fcf6e8fef1fedc45b3b7f35757354b08ddb
Juanda <becariograubassas@gmail.com>
2026-01-08 12:54:04 +0000

    Aviso Legal page and footer fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from 'react-router-dom'
-import Marquee from './components/Marquee'
-import { marqueeItems } from './data/MarqueeData'
-import Header from './components/Header'
-import Home from './pages/Home'
-import Product from './pages/Product'
-import ProductDetails from './components/ProductDetails'
-import Contact from './pages/Contact'
-import Footer from './components/Footer'
+import { Routes, Route } from "react-router-dom";
+import Marquee from "./components/Marquee";
+import { marqueeItems } from "./data/MarqueeData";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductDetails from "./components/ProductDetails";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import AvisoLegal from "./pages/AvisoLegal";
 
 function App() {
   return (
@@ -19,11 +20,12 @@ function App() {
         <Route path="/products/:title" element={<ProductDetails />} />
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/aviso-legal" element={<AvisoLegal />} />
       </Routes>
-      
+
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
