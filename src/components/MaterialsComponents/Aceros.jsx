532ccba7b26d8ffba74a02319a449abe2ce3015c
Juanda <becariograubassas@gmail.com>
2026-01-09 11:20:57 +0000

    aceros logic 2.0

--- src/components/MaterialsComponents/Aceros.jsx
+++ src/components/MaterialsComponents/Aceros.jsx
@@ -1,63 +1,59 @@
 import React from "react";
 import Hero from "../HeroComponents/Hero";
-import { menuData } from "../../data/MenuData";
+import { productsData } from "../../data/ProductsData";
 
 const Aceros = () => {
   const heroTile = "Aceros";
   const heroDescription =
     "Descubre nuestro catálogo de aceros, donde reunimos una amplia selección de calidades y aplicaciones pensadas para distintos sectores. Aquí encontrarás una muestra de los productos disponibles en nuestros almacenes, cuidadosamente seleccionados para garantizar calidad y disponibilidad. ";
 
-  const aceroGroups = menuData
-    .find((item) => item.label === "Productos")
-    ?.children.find((item) => item.label === "Aceros")?.children;
-  console.log(aceroGroups);
+  const productosCategory = productsData.find(
+    (item) => item.label === "Productos"
+  );
+  const aceroData = productosCategory?.children.find(
+    (item) => item.label === "Aceros"
+  );
+  const gruposAcero = aceroData?.children || [];
+  const iconMap = {
+    AlCarbono: "precision_manufacturing",
+    Cementacion: "heat_pump",
+    Resistencia: "fitness_center",
+    Rodamientos: "settings_backup_restore",
+    AltaElasticidad: "spring",
+    Nituración: "science",
+    Herramientas: "construction",
+  };
 
   return (
     <>
       <Hero title={heroTile} description={heroDescription} />
-      <div className="mx-auto mb-10">
-        <h1 className="text-2xl font-bold text-center mb-6">
-          Aceros al Carbono
-        </h1>
-        <div className="flex flex-wrap gap-6 justify-center flex">
-          <a
-            href="/products/F-1110"
-            className="bg-gray-200 rounded-md p-2 hover:bg-primary hover:text-white"
-          >
-            F-1110
-          </a>
-          <a
-            href="/products/F-1120"
-            className="bg-gray-200 rounded-md p-2 hover:bg-primary hover:text-white"
-          >
-            F-1120
-          </a>
-          <a
-            href="/products/F-1130"
-            className="bg-gray-200 rounded-md p-2 hover:bg-primary hover:text-white"
-          >
-            F-1130
-          </a>
-          <a
-            href="/products/F-1140"
-            className="bg-gray-200 rounded-md p-2 hover:bg-primary hover:text-white"
-          >
-            F-1140
-          </a>
-          <a
-            href="/products/F-1150"
-            className="bg-gray-200 rounded-md p-2 hover:bg-primary hover:text-white"
-          >
-            F-1150
-          </a>
-          <a
-            href="/products/ST52-3"
-            className="bg-gray-200 rounded-md p-2 hover:bg-primary hover:text-white"
-          >
-            ST52-3
-          </a>
-        </div>
+      <div className="items-center mb-16 text-center flex flex-col">
+        <h2 className="text-4xl font-bold font-display text-scale-900 ">
+          Selector de Calidades
+        </h2>
       </div>
+      <section className="mx-auto mb-10">
+        <div className="mx-auto mb-10">
+          {gruposAcero.map((grupo, index) => (
+            <div key={index} className="mb-6">
+              <h1 className="text-2xl font-bold text-center mb-6">
+                {grupo.label}
+              </h1>
+              <div className="flex justify-center gap-6 ">
+                {grupo.children.map((item, index) => (
+                  <a
+                    key={index}
+                    href={item.href}
+                    className="p-2 border-b border-gray-200 hover:bg-primary hover:text-white rounded-md bg-gray-200"
+                  >
+                    {item.label}
+                  </a>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
