823a8551904752b97770072fc649a1301ee7c74b
Juanda <becariograubassas@gmail.com>
2026-01-27 15:31:44 +0000

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import logo from "../assets/img/imagotipo_GrauBassas_vertical_blanco.webp";
 import { PhoneIcon, EmailIcon, FacebookIcon, LocationIcon } from "./Icons";
+import useIsMobile from "../hooks/useIsMobile";
 
 export default function Footer() {
+  const isMobile = useIsMobile();
+
   return (
     <footer className="bg-primary text-white py-6 relative overflow-hidden">
       <div
@@ -26,44 +29,97 @@ export default function Footer() {
           />
         </div>
 
-        <div className="hidden md:block w-px h-24 bg-white/30 mx-8"></div>
+        {!isMobile && (
+          <div className="hidden md:block w-px h-24 bg-white/30 mx-8"></div>
+        )}
 
-        <div className="flex flex-col gap-4 text-sm font-light">
-          <div className="flex items-center gap-3">
-            <EmailIcon className="text-2xl" />
-            <span>info@graubassas.com</span>
+        {isMobile ? (
+          <div className="grid grid-cols-1 gap-4 max-w-xs w-full mb-5">
+            <div className="flex flex-col-2 justify-between">
+              <div className="flex flex-col gap-2">
+                <EmailIcon className="text-3xl text-white/90" />
+                <span>info@graubassas.com</span>
+              </div>
+              <div className="flex flex-col gap-2">
+                <PhoneIcon className="text-3xl text-white/90" />
+                <span>928460044</span>
+              </div>
+            </div>
+            <div className="flex flex-col-2 justify-between">
+              <div className="flex flex-col gap-2">
+                <LocationIcon className="text-3xl text-white/90" />
+                <a href="https://maps.app.goo.gl/e47h6BHSc3ox4LrJ6">
+                  Calle Prof. Lozano, 18, 35008, Las
+                  <br />
+                  Palmas Gran Canaria, Las Palmas
+                </a>
+              </div>
+              <div className="flex flex-col gap-2">
+                <FacebookIcon className="text-3xl text-white/90" />
+                <a href="https://www.facebook.com/Almacenesgraubassas/?locale=es_ES">
+                  Facebook
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="flex items-center gap-3">
-            <PhoneIcon className="text-2xl" />
-            <span>928460044</span>
-          </div>
-        </div>
+        ) : (
+          <>
+            <div className="flex flex-col gap-4 text-sm font-light">
+              <div className="flex items-center gap-3">
+                <EmailIcon className="text-3xl text-white/90" />
+                <span>info@graubassas.com</span>
+              </div>
+              <div className="flex items-center gap-3">
+                <PhoneIcon className="text-3xl text-white/90" />
+                <span>928460044</span>
+              </div>
+            </div>
 
-        <div className="hidden md:block w-px h-24 bg-white/30 mx-8"></div>
+            <div className="hidden md:block w-px h-24 bg-white/30 mx-8"></div>
 
-        <div className="flex flex-col gap-4 text-sm font-light">
-          <div className="flex items-start gap-3">
-            <LocationIcon className="text-2xl" />
-            <a href="https://maps.app.goo.gl/e47h6BHSc3ox4LrJ6">
-              Calle Prof. Lozano, 18, 35008, Las
-              <br />
-              Palmas Gran Canaria, Las Palmas
-            </a>
-          </div>
-          <div className="flex items-center gap-3">
-            <FacebookIcon className="text-2xl" />
-            <span>Facebook</span>
-          </div>
-        </div>
+            <div className="flex flex-col gap-4 text-sm font-light">
+              <div className="flex items-start gap-3">
+                <LocationIcon className="text-3xl text-white/90" />
+                <a href="https://maps.app.goo.gl/e47h6BHSc3ox4LrJ6">
+                  Calle Prof. Lozano, 18, 35008, Las
+                  <br />
+                  Palmas Gran Canaria, Las Palmas
+                </a>
+              </div>
+              <div className="flex items-center gap-3">
+                <FacebookIcon className="text-3xl text-white/90" />
+                <a href="https://www.facebook.com/Almacenesgraubassas/?locale=es_ES">
+                  Facebook
+                </a>
+              </div>
+            </div>
+          </>
+        )}
       </div>
 
-      <div className="container mx-auto mt-2 pt-1 border-t border-white/20 text-center text-xs text-white/60 flex justify-between">
-        <div className="space-x-4">
-          <a href="/contacto">Contacto</a> |{" "}
-          <a href="/aviso-legal">Aviso Legal</a> |{" "}
-          <a href="#">Terminos y Condiciones</a>
+      <div
+        className={` border-t border-white/20 pt-2 text-center text-xs ${
+          isMobile
+            ? "flex flex-col gap-4 text-center borde mt-2"
+            : "container mx-auto flex justify-between mt-2"
+        }`}
+      >
+        <div className="space-x-4 text-white/60">
+          <a href="/contacto" className="hover:text-white transition-colors">
+            Contacto
+          </a>
+          <span>|</span>
+          <a href="/aviso-legal" className="hover:text-white transition-colors">
+            Aviso Legal
+          </a>
+          <span>|</span>
+          <a href="#" className="hover:text-white transition-colors">
+            Terminos y Condiciones
+          </a>
         </div>
-        <div className="text-white">Copyright © 2026 | GRAU BASSAS S.L.</div>
+        <div className="text-white">
+          Copyright © {new Date().getFullYear()} | GRAU BASSAS S.L.
+        </div>
       </div>
     </footer>
   );
